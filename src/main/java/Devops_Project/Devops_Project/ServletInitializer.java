23362f01c717523353593844c7ac8b56532ce164
ashwini0524 <nimmakayala.ashwini@gmail.com>
2025-11-29 21:42:36 +0530

    Devops Project

---
+++ src/main/java/Devops_Project/Devops_Project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Devops_Project.Devops_Project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DevopsProjectApplication.class);
+	}
+
+}
---
+++ src/test/java/Devops_Project/Devops_Project/DevopsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Devops_Project.Devops_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
